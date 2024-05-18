105f7bc793d2944372ad5e4569562ad0d4b6e7d8
OUSMANE MERIGA <ousmane.meriga12@gmail.com>
2024-05-18 20:23:59 +0100

    Constructor

--- learn_dart/bin/learn_dart.dart
+++ learn_dart/bin/learn_dart.dart
@@ -5,17 +5,49 @@ class Personne{
 String? nom;
 String? prenom;
 
+  Personne(String nom, String prenom){
+    this.nom = nom;
+    this.prenom = prenom;
+  }
+
 void greetings(){
   print("Je suis $nom $prenom");
 }
 
 }
 
+class Camera{
+  String? name;
+  String? color;
+  double? megaPixels;
+
+
+  void display(){
+    print("la camera $name de couleur $color a $megaPixels MegaPixels");
+  }
+}
+
 void main()
 {
-Personne personne = Personne();
-personne.nom = 'OUSMANE';
-personne.prenom ='MEIRGA';
-personne.greetings();
+// Personne personne = Personne();
+// personne.nom = 'OUSMANE';
+// personne.prenom ='MEIRGA';
+// personne.greetings();
+
+Personne personne2 = Personne("BABANA", "MERIGA");
+personne2.greetings();
+
+
+// Camera camera1 = Camera();
+// camera1.name = 'CANON';
+// camera1.color = 'Bleue';
+// camera1.megaPixels = 12.5;
+// camera1.display();
+
+// Camera camera2 = Camera();
+// camera2.name = 'KODAK';
+// camera2.color = 'verte';
+// camera2.megaPixels = 16.3;
+// camera2.display();
 
 }
